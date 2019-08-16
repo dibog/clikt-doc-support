6f4ec9a0c1dda65707f789a6db09ca781046a21f
Dieter Bogdoll <dibog@github.com>
2019-08-16 23:43:19 +0200

    added how to document your commands

--- src/test/java/io/github/dibog/cliktdoc/TestDemoCmdDoku.kt
+++ src/test/java/io/github/dibog/cliktdoc/TestDemoCmdDoku.kt
@@ -16,10 +16,17 @@ class TestDemoCmdDoku : CliKtDocSupport {
         clearAllMocks()
     }
 
+    // please don't change the block (also not the spacing, otherwise the asciidoc would look messy
+    // tag::demo-command[]
     @Test
-    fun testDemo() = document(listOf(), "normal") {
-
+    fun testDemo() = document(                              // <1>
+            listOf(),                                       // <2>
+            "demo"                                          // <3>
+    ) {
+        // optional mock specs can go here                  // <4>
+        // the block can be empty or be not there at all
     }
+    // end::demo-command[]
 
     fun document(cmdArgs: List<String>, fragmentName: String, action: ()->Unit = {}) {
         document(cmd, baseArgs, cmdArgs, fragmentName, action)
