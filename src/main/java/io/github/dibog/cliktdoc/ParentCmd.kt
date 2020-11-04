2ec4f36cff87ee96a6d6eedcf8648c65ac18644f
dibog <dieter@bogdoll.net>
2020-11-04 08:43:43 +0100

    updated to clikt 3.0.1

--- src/main/java/io/github/dibog/cliktdoc/ParentCmd.kt
+++ src/main/java/io/github/dibog/cliktdoc/ParentCmd.kt
@@ -1,11 +1,11 @@
 package io.github.dibog.cliktdoc
 
-import com.github.ajalt.clikt.core.NoRunCliktCommand
+import com.github.ajalt.clikt.core.NoOpCliktCommand
 import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.output.CliktHelpFormatter
 
-class ParentCmd : NoRunCliktCommand("""
+class ParentCmd : NoOpCliktCommand("""
     Description for this command""".trimIndent(),
         name="parent")
 {
